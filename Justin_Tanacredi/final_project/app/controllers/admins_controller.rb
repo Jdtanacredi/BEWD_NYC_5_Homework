49e5ab6c34e86ee3002a69b0826c3429fc723d94
Justin Tanacredi <Jtanacredi@gmail.com>
2013-12-08 16:54:14 -0500

    remove specific items controller. IT WAS USELESS

--- Justin_Tanacredi/final_project/app/controllers/admins_controller.rb
+++ Justin_Tanacredi/final_project/app/controllers/admins_controller.rb
@@ -1,7 +1,6 @@
 class AdminsController < ApplicationController
   before_action :is_admin?, only: [:edit, :show, :update, :create, :destroy]
 
-
   def is_admin?
     current_user = session[:user_id]
     unless current_user.admin?
--- Justin_Tanacredi/final_project/app/controllers/specific_items_controller.rb
+++
@@ -1,7 +0,0 @@
-class SpecificItemsController < ApplicationController
-  before_action :is_admin?, only: [:edit, :show, :update, :create, :destroy]
-
-  def is_admin?
-
-  end
-end
